f91a6b9ab7d7ef4fb84d01cc5d44389c78c855a2
lodjim <lodjim5@gmail.com>
2026-02-24 11:17:53 +0000

    add a constructor for constraint

--- simple_token_contract.sol
+++ simple_token_contract.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.28;
 
 contract SimpleToken {
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     mapping(address => uint256) public balances;
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Mint(address indexed to, uint256 value);
@@ -17,12 +23,18 @@ contract SimpleToken {
         _;
     }
 
-    function mint(address to, uint256 amount) external onlyPositive(amount) {
+    modifier onlyOwnerCan(){
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function mint(address to, uint256 amount) external onlyPositive(amount) onlyOwnerCan() {
+        
         balances[to] += amount;
         emit Mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) external onlyPositive(amount) onlyEnough(from, amount) {
+    function burn(address from, uint256 amount) external onlyPositive(amount) onlyEnough(from, amount) onlyOwnerCan() {
         balances[from] -= amount;
         emit Burn(from, amount);
     }
